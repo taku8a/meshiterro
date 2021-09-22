635942e63681b1a0fe024d439d4897d0d3a01e47
taku8a <takuya0505hsmt@gmail.com>
2021-09-22 02:12:49 +0000

    second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b703964982291094937b70998d4ffae624c9fd86632cfd072c89d4ff91129836fa7619fd6ff9b77cf8ca91ce777726cc065367f21fc5e231fb2474a5c93e8bb2'
